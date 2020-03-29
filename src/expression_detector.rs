caefd83f9c65ec3628c8c6ced6f60dac94235a3a
Joseph Catrambone <jo.jcat@gmail.com>
2020-03-28 18:39:49 -0700

    Some changes to how the program was structured.  Start working on the expression detector.

--- src/expression_detector.rs
+++ src/expression_detector.rs
@@ -1,67 +1,74 @@
 
 use nalgebra::linalg::SVD;
 use nalgebra::{DMatrix, Vector, DVector};
+use serde::{Deserialize, Serialize};
 
 const LATENT_SIZE:usize = 512;
+const IMAGE_WIDTH:usize = 64;
+const IMAGE_HEIGHT:usize = 64;
 
-/*
-	let dm1 = DMatrix::from_diagonal_element(4, 3, 1.0);
-    let dm2 = DMatrix::identity(4, 3);
-    let dm3 = DMatrix::from_fn(4, 3, |r, c| if r == c { 1.0 } else { 0.0 });
-    let dm4 = DMatrix::from_iterator(
-        4,
-        3,
-        [
-            // Components listed column-by-column.
-            1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0,
-        ]
-        .iter()
-        .cloned(),
-    );
-*/
-
+#[derive(Serialize, Deserialize)]
 struct Expression {
 	name: String,
-	latent_point: [f32; LATENT_SIZE],
+	latent_point: Vec<Vec<f32>>,
 }
 
-struct ExpressionGroup<'a> {
-	expressions: Vec<&'a Expression>,
+#[derive(Serialize, Deserialize)]
+struct ExpressionGroup {
+	name: String,
+	expressions: Vec<Expression>,
 	mutually_exclusive: bool,  // If true, only one of these can be active at a time.
 }
 
-pub struct ExpressionDetector<'a> {
-	expressions: Vec<Expression>,
-	expression_groups: Vec<ExpressionGroup<'a>>,
+#[derive(Serialize, Deserialize)]
+pub struct ExpressionDetector {
+	expression_groups: Vec<ExpressionGroup>,
 	mean_face: Vec<f32>,
+	selected_group: Option<ExpressionGroup>,
+	selected_expression: Option<Expression>,
 	//face_profile: DMatrix<f32>,
 	// low-dim = sigma_inv * U_trans * q
 }
 
-impl<'a> ExpressionDetector<'_> {
-	pub fn new_from_face_images(image_width: u32, image_height: u32, image_data:&Vec<Vec<u8>>) -> Self {
-		// Given a bunch of captures of a face, compute the eigenfaces and build a dimensionality reduction matrix so we can get a latent space.
-		let mut mean_face = vec![0f32; (image_width*image_height) as usize];
-		for img_data in image_data {
-			for (i, pixel_data) in img_data.iter().enumerate() {
-				mean_face[i] += (*pixel_data) as f32 / 255f32;
-			}
-		}
-		
-		// Now that we have a bunch of face captures, build a matrix from them.  Each row should be a face.
-		let mut face_matrix = DMatrix::from_fn(image_data.len(), (image_width*image_height) as usize, |r, c|{
-			image_data[r][c] as f32/255f32 - mean_face[c]
-		});
-		let svd = SVD::new(face_matrix, true, true); // TODO: use try_new.
-		//let u = svd.u.unwrap();
-		//let s = svd.singular_values;
-		//let v = svd.v_t.unwrap().transpose();
-		
+impl ExpressionDetector {
+	pub fn new() -> Self {
 		ExpressionDetector {
-			expressions: vec![],
 			expression_groups: vec![],
-			mean_face,
-			//DMatrix::identity
+			mean_face: vec![],
+			selected_group: None,
+			selected_expression: None,
 		}
+	}
+	
+	pub fn get_expression_list(&self) -> Vec<(String, Vec<String>)> {
+		let mut expression_list = vec![];
+		
+		for xg in &self.expression_groups {
+			let mut subexpr = vec![];
+			for xpr in &xg.expressions {
+				subexpr.push(xpr.name.clone());
+			}
+			expression_list.push((xg.name.clone(), subexpr));
+		}
+		
+		expression_list
+	}
+	
+	pub fn get_expression_weights(&self, image_width:usize, image_height:usize, image_data:&Vec<u8>, roi:(u32, u32, u32, u32)) -> Vec<(String, Vec<(String, f32)>)> {
+		// Returns a list of { group: [expression 1: amount, expression 2: amount, expression 3: amount, ...], group 2: [expression 1: amount, ...]
+		let mut expression_list = vec![];
+		
+		// Extract the ROI from the given face.
+		todo!();
+		
+		for xg in &self.expression_groups {
+			let mut subexpr = vec![];
+			for xpr in xg.expressions {
+				subexpr.push((xpr.name, 0f32));
+			}
+			expression_list.push((xg.name.clone(), subexpr));
+		}
+		
+		expression_list
 	}
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod haar_like_feature;
 mod integral_image;
 mod decision_tree;
 
+use expression_detector::ExpressionDetector;
 use face_detector::FaceDetector;
 
 use camera_capture::ImageIterator;
@@ -11,8 +12,7 @@ use camera_capture::ImageIterator;
 use image::ImageBuffer;
 use macroquad::*;
 use macroquad::Image as MQImage;
-use std::borrow::BorrowMut;
-use std::cell::Cell;
+//use std::cell::Cell;
 //use std::cmp::max;
 use std::str::FromStr;
 use std::sync::Arc;
@@ -20,6 +20,7 @@ use std::sync::mpsc::{channel, Sender, Receiver};
 use std::time::{SystemTime, UNIX_EPOCH};
 
 const COLOR_CHANNELS:usize = 4;
+const FONT_SIZE:f32 = 16f32;
 
 enum ApplicationModes {
 	SetupCamera,
@@ -34,6 +35,9 @@ struct ApplicationState {
 	bw_image_source: MQImage,
 	cpu_image_buffer: MQImage,
 	gpu_image: Texture2D,
+	
+	expression_detector: ExpressionDetector,
+	
 	mode: ApplicationModes,
 }
 
@@ -48,7 +52,7 @@ async fn main() {
 	let mut frame_iter = open_webcam(0, (camera_resolution.0 as u32, camera_resolution.1 as u32));
 	
 	// Start application state.
-	let mut application_state = Cell::new(init_application(camera_resolution));
+	let mut application_state = init_application(camera_resolution);
 	
 	let fd: FaceDetector = serde_json::from_str(include_str!("../face_detector.json")).unwrap();
 	//let fd = FaceDetector::new();
@@ -83,6 +87,24 @@ async fn main() {
 		}
 		*/
 		
+		// Branch based on application logic.
+		{
+			match application_state.mode {
+				ApplicationModes::Preview => {
+					preview(&mut application_state);
+				},
+				ApplicationModes::SetupCamera => {
+					setup_camera(&mut application_state);
+				},
+				ApplicationModes::SetupExpression => {
+					setup_expression(&mut application_state);
+				},
+				ApplicationModes::Recording => {
+				
+				}
+			}
+		}
+		
 		// Count the number of frames we've done and drop them if we've taken too long.
 		let frame_time = frame_start_time.elapsed();
 		if let Ok(dt) = frame_time {
@@ -95,13 +117,25 @@ async fn main() {
 		}
 		
 		rolling_average_dropped_frames = 0.9*rolling_average_dropped_frames + 0.1*dropped_frames as f32;
-		draw_text(format!("Avg dropped frames: {}", rolling_average_dropped_frames).as_str(), 10f32, 10f32, 16f32, WHITE);
+		draw_text(format!("Avg dropped frames: {}", rolling_average_dropped_frames).as_str(), 10f32, 10f32, FONT_SIZE, WHITE);
 		dropped_frames = 0;
 		
 		next_frame().await
 	}
 
 	//camera_thread.join().unwrap();
+}
+
+fn parse_pair<T>(s:&str, separator:char) -> Option<(T, T)> where T: FromStr {
+	match s.find(separator) {
+		None => None,
+		Some(index) => {
+			match(T::from_str(&s[..index]), T::from_str(&s[index+1..])) {
+				(Ok(a), Ok(b)) => Some((a, b)),
+				_ => None
+			}
+		}
+	}
 }
 
 fn open_webcam(i:u32, camera_resolution: (u32, u32)) -> ImageIterator {
@@ -128,23 +162,10 @@ fn open_webcam(i:u32, camera_resolution: (u32, u32)) -> ImageIterator {
 	frame_iter
 }
 
-fn parse_pair<T>(s:&str, separator:char) -> Option<(T, T)> where T: FromStr {
-	match s.find(separator) {
-		None => None,
-		Some(index) => {
-			match(T::from_str(&s[..index]), T::from_str(&s[index+1..])) {
-				(Ok(a), Ok(b)) => Some((a, b)),
-				_ => None
-			}
-		}
-	}
-}
-
-fn update_capture_data(application_state: &mut Cell<ApplicationState>, frame_iter:&mut ImageIterator) {
+fn update_capture_data(application_state: &mut ApplicationState, frame_iter:&mut ImageIterator) {
 	if let Some(mut camera_frame) = frame_iter.next() { // receiver.try_recv() {
-		let mut astate = application_state.get_mut();
-		let mut image = &mut astate.cpu_image_buffer;
-		let mut bw_image = &mut astate.bw_image_source;
+		let mut image = &mut application_state.cpu_image_buffer;
+		let mut bw_image = &mut application_state.bw_image_source;
 		for y in 0..image.height as u32 {
 			for x in 0..image.width as u32 {
 				let i = (x + y * image.width as u32) as usize * COLOR_CHANNELS;
@@ -156,12 +177,12 @@ fn update_capture_data(application_state: &mut Cell<ApplicationState>, frame_ite
 				bw_image.bytes[i/COLOR_CHANNELS] = ((pxl[0] as u32 + pxl[1] as u32 + pxl[2] as u32) / 3) as u8; // TODO: Proper color interp.
 			}
 		}
-		update_texture(astate.gpu_image, &image);
+		update_texture(application_state.gpu_image, &image);
 	}
 }
 
-fn draw_capture_frame(application_state: &mut Cell<ApplicationState>) {
-	let texture = application_state.get_mut().gpu_image;
+fn draw_capture_frame(application_state: &mut ApplicationState) {
+	let texture = application_state.gpu_image;
 	draw_texture(
 		texture,
 		screen_width() / 2. - texture.width() / 2.,
@@ -190,22 +211,98 @@ fn init_application(camera_resolution:(usize, usize)) -> ApplicationState {
 		bw_image_source: bw_image,
 		cpu_image_buffer: image,
 		gpu_image: texture,
+		expression_detector: ExpressionDetector::new(),
 		mode: ApplicationModes::Preview
 	}
 }
 
-fn preview() {
-
+fn handle_mode_switch_keys(application_state:&mut ApplicationState) {
+	if is_key_down(KeyCode::Q) {
+		application_state.mode = ApplicationModes::Preview;
+	} else if is_key_down(KeyCode::W) {
+		application_state.mode = ApplicationModes::SetupCamera;
+	} else if is_key_down(KeyCode::E) {
+		application_state.mode = ApplicationModes::SetupExpression;
+	} else if is_key_down(KeyCode::R) {
+		application_state.mode = ApplicationModes::Recording;
+	}
 }
 
-fn setup_camera() {
-
+fn preview(application_state:&mut ApplicationState) {
+	// Draw the frame
+	let texture = application_state.gpu_image;
+	let x_offset = screen_width() / 2.0 - texture.width() / 2.0;
+	let y_offset = screen_height() / 2.0 - texture.height() / 2.0;
+	draw_texture(texture, x_offset, y_offset, WHITE);
+	
+	draw_text("PREVIEW MODE -- [W]: Setup Camera  [E]: Setup Expresion  [R]: Record", 10f32, screen_height()-16f32, FONT_SIZE, Color([255, 255, 255, 255]));
+	handle_mode_switch_keys(application_state);
 }
 
-fn recording() {
-
+fn setup_camera(application_state:&mut ApplicationState) {
+	// Draw the frame
+	let texture = application_state.gpu_image;
+	let x_offset = screen_width() / 2.0 - texture.width() / 2.0;
+	let y_offset = screen_height() / 2.0 - texture.height() / 2.0;
+	draw_texture(texture, x_offset, y_offset, WHITE);
+	
+	// Draw some dark boundaries around the image's ROI.
+	// Rather than draw a rectangle directly we draw dark areas _around_ the region of interest.
+	let shade_color = Color([0, 0, 0, 100]);
+	let roi_x = application_state.focus_area.0 as f32 + x_offset;
+	let roi_y = application_state.focus_area.1 as f32 + y_offset;
+	let roi_w = application_state.focus_area.2 as f32;
+	let roi_h = application_state.focus_area.3 as f32;
+	draw_rectangle(0f32, 0f32, screen_width(), roi_y, shade_color); // Top
+	draw_rectangle(0f32, 0f32, roi_x, screen_height(), shade_color); // Left boundary
+	draw_rectangle(roi_x + roi_w, 0f32, (screen_width() - roi_w), screen_height(), shade_color); // Right shade
+	draw_rectangle(0f32, roi_y + roi_h, screen_width(), (screen_height() - roi_h), shade_color); // Bottom shade
+	
+	// Show help text.
+	draw_text("Setting ROI.  [LEFT MOUSE]: Top Left.  [RIGHT MOUSE]: Bottom Right.  [ESCAPE]: Back.", 16f32, screen_height()-24f32, FONT_SIZE, Color([255, 255, 255, 255]));
+	
+	// Maybe update our shaded area.
+	if is_mouse_button_down(MouseButton::Left) || is_mouse_button_down(MouseButton::Right){
+		let (x,y) = mouse_position(); // X and Y are in terms of the screen size.  Remove the offset of the drawing before making them our ROI.
+		let x = (x - x_offset) as u32;
+		let y = (y - y_offset) as u32;
+		
+		if is_mouse_button_down(MouseButton::Left) {
+			application_state.focus_area = (x, y, application_state.focus_area.2, application_state.focus_area.3);
+		} else if is_mouse_button_down(MouseButton::Right) {
+			application_state.focus_area = (application_state.focus_area.0, application_state.focus_area.1, x - application_state.focus_area.0, y - application_state.focus_area.1);
+		}
+	}
+	
+	// Check keyboard input.
+	handle_mode_switch_keys(application_state);
+	if is_key_down(KeyCode::Escape) {
+		application_state.mode = ApplicationModes::Preview;
+	}
 }
 
-fn expression_setup() {
+fn setup_expression(application_state:&mut ApplicationState) {
+	// Draw the frame
+	let texture = application_state.gpu_image;
+	let x_offset = 10f32;
+	let y_offset = 10f32;
+	draw_texture_rec(texture, x_offset, y_offset, application_state.focus_area.2 as f32, application_state.focus_area.3 as f32, application_state.focus_area.0 as f32, application_state.focus_area.1 as f32, application_state.focus_area.2 as f32, application_state.focus_area.3 as f32, WHITE);
+	
+	// Show help text.
+	draw_text("[A] Add Expression, [S] Add Sample, [D] Delete Expression", 16f32, screen_height()-24f32, FONT_SIZE, Color([255, 255, 255, 255]));
+	
+	// Draw all the expressions.
+	
+	
+	// Check for clicks on the mouse.
+	
+	// Check keyboard input.
+	handle_mode_switch_keys(application_state);
+	if is_key_down(KeyCode::Escape) {
+		application_state.mode = ApplicationModes::Preview;
+	}
+}
+
+fn recording(application_state:&mut ApplicationState) {
 
 }
