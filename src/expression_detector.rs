ab8f7b698cfb2de2bf090eb5650ec7632716fe18
Joseph Catrambone <jo.jcat@gmail.com>
2020-06-06 11:19:28 -0700

    Checkpointing on the mess I've mde with Tract/ONNX.  Going back to PyTorch/Tch.

--- src/expression_detector.rs
+++ src/expression_detector.rs
@@ -1,25 +1,25 @@
 
 use std::io::Cursor;
 use serde::{Deserialize, Serialize};
-use tch;
 use std::collections::HashMap;
-
-// Required imports:
-use tch::nn::Module;
+use tract_core;
+use tract_ndarray;
+use tract_onnx;
+use tract_onnx::prelude::*;
 
 // Keep these compatible with the expression detector.
 const LATENT_SIZE:usize = 1024;
-const IMAGE_WIDTH:usize = 48;
-const IMAGE_HEIGHT:usize = 48;
+const DETECTOR_WIDTH:usize = 48;
+const DETECTOR_HEIGHT:usize = 48;
 
 #[derive(Serialize, Deserialize)]
 struct Expression {
 	name: String,
-	latent_point: Vec<f64>,
+	latent_point: Vec<f32>,
 }
 
 pub struct ExpressionDetector {
-	model: tch::CModule,
+	model: SimplePlan<tract_core::model::fact::TypedFact, Box<dyn tract_core::ops::TypedOp>, tract_core::model::model::ModelImpl<tract_core::model::fact::TypedFact, std::boxed::Box<dyn tract_core::ops::TypedOp>>>,
 	expressions: Vec<Expression>,
 	//face_profile: DMatrix<f32>,
 	// low-dim = sigma_inv * U_trans * q
@@ -27,37 +27,26 @@ pub struct ExpressionDetector {
 
 impl ExpressionDetector {
 	pub fn new() -> Self {
-		let mut static_model_data = include_bytes!("../ml/expression_detector_cexport.pt");
+		let mut static_model_data = include_bytes!("../ml/expression_detector_cpu.onnx");
+		let mut cursor = Cursor::new(static_model_data);
+		let m = onnx()
+			.model_for_read(&mut cursor).unwrap()
+			.with_input_fact(0, InferenceFact::dt_shape(f32::datum_type(), tvec!(1, 1, DETECTOR_HEIGHT, DETECTOR_WIDTH))).unwrap()
+			.into_optimized().unwrap()
+			.into_runnable().unwrap();
 		
-		let module = tch::CModule::load_data(&mut Cursor::new(&static_model_data[..]));
-		match module {
-			Ok(m) => {
-				ExpressionDetector {
-					model: m,
-					expressions: vec![]
-				}
-			},
-			Err(e) => {
-				eprintln!("{}", e);
-				panic!(e);
-			}
+		ExpressionDetector {
+			model: m,
+			expressions: vec![]
 		}
 	}
 	
 	pub fn add_expression(&mut self, image_width:u32, image_height:u32, image_data:&Vec<u8>, roi:(u32, u32, u32, u32), name:String) {
 		// Cut the region out, resize it to our face detector.
-		let mut face:Vec<f64> = image_and_roi_to_vec(image_width, image_height, image_data, roi).iter().map(|f|{ *f as f64 / 255.0f64 }).collect();
-		let mut tensor = tch::Tensor::of_slice(face.as_slice()).view([1, 1, IMAGE_HEIGHT as i64, IMAGE_WIDTH as i64]);
+		let mut face = image_and_roi_to_tensor(image_width, image_height, image_data, roi);
 		
 		// Calculate embedding.
-		//let t = tch::Tensor::of_slice(face.as_slice());
-		//let t = IValue::DoubleList(face);
-		let mut embedding:Vec<f64> = Vec::<f64>::from(self.model.forward(&tensor));
-		/*
-		tch::no_grad(||{
-			embedding = Vec::<f64>::from(self.model.forward(&tensor));
-		});
-		 */
+		let embedding = (self.model.run(tvec!(face)) as TractResult<()>).unwrap()[0].to_array_view::<f32>().unwrap();
 		
 		// Insert a new expression.
 		self.expressions.push(Expression{
@@ -83,20 +72,19 @@ impl ExpressionDetector {
 		let mut expression_list = HashMap::with_capacity(self.expressions.len());
 		
 		// Extract the ROI from the given face.
-		let mut face:Vec<f64> = image_and_roi_to_vec(image_width, image_height, image_data, roi).iter().map(|f|{ *f as f64 / 255.0f64 }).collect();
-		let mut tensor = tch::Tensor::of_slice(face.as_slice()).view([1, 1, IMAGE_HEIGHT as i64, IMAGE_WIDTH as i64]);
+		let mut face = image_and_roi_to_tensor(image_width, image_height, image_data, roi);
 		
 		// Embed the extracted face:
-		let embedding:Vec<f64> = Vec::<f64>::from(self.model.forward(&tensor));
-		let mut embedding_magnitude = 0f64;
+		let embedding = (self.model.run(tvec!(face)) as TractResult<()>).unwrap()[0].to_array_view::<f32>().unwrap();
+		let mut embedding_magnitude = 0f32;
 		for i in 0..LATENT_SIZE {
-			embedding_magnitude += (embedding[i]*embedding[i]) as f64;
+			embedding_magnitude += (embedding[i]*embedding[i]) as f32;
 		}
 		
 		// Calc cosine product with all expressions.
 		for xpr in &self.expressions {
-			let mut similarity = 0f64;
-			let mut magnitude = 0.0f64;
+			let mut similarity = 0f32;
+			let mut magnitude = 0.0f32;
 			for (idx, a) in xpr.latent_point.iter().enumerate() {
 				let a = *a;
 				let b = embedding[idx];
@@ -116,11 +104,11 @@ fn image_and_roi_to_vec(image_width:u32, image_height:u32, image_data:&Vec<u8>, 
 	// Calculate the mapping from x/y in the final image to x/y in the ROI of the source image.
 	// x goes from 0 -> IMAGE_WIDTH.  We want it to go from 0 -> ROI_w.
 	// x * ROI_w/IMAGE_WIDTH + roi_x
-	let x_to_src = roi.2 as f32 / IMAGE_WIDTH as f32;
-	let y_to_src = roi.3 as f32 / IMAGE_HEIGHT as f32;
+	let x_to_src = roi.2 as f32 / DETECTOR_WIDTH as f32;
+	let y_to_src = roi.3 as f32 / DETECTOR_HEIGHT as f32;
 	
-	for y in 0..IMAGE_HEIGHT {
-		for x in 0..IMAGE_WIDTH {
+	for y in 0..DETECTOR_HEIGHT {
+		for x in 0..DETECTOR_WIDTH {
 			let src_x = (x as f32 * x_to_src) as usize + roi.0 as usize;
 			let src_y = (y as f32 * y_to_src) as usize + roi.1 as usize;
 			result.push(image_data[src_x + src_y*image_width as usize]);
@@ -128,4 +116,15 @@ fn image_and_roi_to_vec(image_width:u32, image_height:u32, image_data:&Vec<u8>, 
 	}
 	
 	result
+}
+
+// Given a source image of the form w,h,data and an roi with (x, y, w, h), extract a tensor.
+fn image_and_roi_to_tensor(image_width:u32, _image_height:u32, image_data:&Vec<u8>, roi:(u32, u32, u32, u32)) -> Tensor {
+	let x_to_src = roi.2 as f32 / DETECTOR_WIDTH as f32;
+	let y_to_src = roi.3 as f32 / DETECTOR_HEIGHT as f32;
+	tract_ndarray::Array4::from_shape_fn((1, 1, DETECTOR_HEIGHT, DETECTOR_WIDTH), |(_b, c, y, x)| {
+		let src_x = (x as f32 * x_to_src) as usize + roi.0 as usize;
+		let src_y = (y as f32 * y_to_src) as usize + roi.1 as usize;
+		image_data[src_x + src_y*image_width as usize] as f32 / 255.0f32
+	}).into()
 }
